21fafccacb46a1962563f2c640b14e539f433541
Hellymaw <aaron@helmore.org>
2024-07-14 15:13:45 +1000

    Nicer type conversion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-use std::io;
 use rand::Rng;
+use std::{fmt, io};
 use Guessable::*;
 
 #[derive(Debug)]
 enum Outcome {
     Tied,
     Won,
-    Lost
+    Lost,
+}
+
+impl fmt::Display for Outcome {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let text = match self {
+            Self::Tied => "tied",
+            Self::Won => "won",
+            Self::Lost => "lost",
+        };
+
+        write!(f, "{}", text)
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -34,42 +46,69 @@ impl Guessable {
             Guessable::Paper => Guessable::Rock,
         }
     }
+}
 
+impl fmt::Display for Guessable {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let text = match self {
+            Rock => "Rock",
+            Paper => "Paper",
+            Scissors => "Scissors",
+        };
+
+        write!(f, "{}", text)
+    }
+}
+
+impl From<u8> for Guessable {
+    fn from(value: u8) -> Self {
+        match value % 3 {
+            0 => Rock,
+            1 => Paper,
+            _ => Scissors,
+        }
+    }
+}
+
+impl TryFrom<&str> for Guessable {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "rock" => Ok(Rock),
+            "paper" => Ok(Paper),
+            "scissors" => Ok(Scissors),
+            x => Err(x.to_string()),
+        }
+    }
 }
 
 fn main() {
     println!("Welcome to rock-paper-scissors-rs!");
 
     loop {
+        println!("");
         println!("What do you pick? Rock, Paper or Scissors? (Press CTRL+C to exit)");
-    
-        let mut user_pick = String::new();
 
-        let pc_pick = rand::thread_rng().gen_range(1..=3);
-        let pc_pick = match pc_pick {
-            1 => Rock,
-            2 => Paper, 
-            3 => Scissors,
-            _ => panic!("This isn't possible")
-        };
-        
+        let mut user_pick = String::new();
         io::stdin()
             .read_line(&mut user_pick)
             .expect("Failed to read line");
 
-        let selected = match user_pick.to_lowercase().as_str().trim() {
-            "rock" => Rock,
-            "paper" => Paper,
-            "scissors" => Scissors,
-            x => {
-                println!("{} is not a valid pick, try again...", x);
+        let selected = match user_pick.trim().try_into() {
+            Ok(x) => x,
+            Err(e) => {
+                println!("{} is not a valid pick, try again...", e);
                 continue;
             }
         };
 
-        println!("Your pick: {:?}, PC pick: {:?}", selected, pc_pick);
-        
+        let pc_pick: u8 = rand::thread_rng().gen();
+        let pc_pick = pc_pick.into();
+
+        println!("Your pick: {}, PC pick: {}", selected, pc_pick);
+
         let winner = Guessable::winner(selected, pc_pick);
-        println!("You {:?}!", winner);
+        println!("You {}!", winner);
     }
 }
